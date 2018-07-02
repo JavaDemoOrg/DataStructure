258fa571754b7432951406083de65c9b55e58bb9
huangweiqi <askviky2010@gmail.com>
2018-07-02 23:40:06 +0800

    init repo

---
+++ app/src/main/java/com/abt/structure/DoAndroid.java
@@ -0,0 +1,9 @@
+package com.abt.structure;
+
+/**
+ * @描述： @DoAndroid
+ * @作者： @黄卫旗
+ * @创建时间： @16/06/2018
+ */
+class DoAndroid {
+}
---
+++ app/src/main/java/com/abt/structure/ui/MainActivity.java
@@ -0,0 +1,22 @@
+package com.abt.structure.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.abt.structure.hash_map.DoHashMap;
+
+/**
+ * @描述： @MainActivity
+ * @作者： @黄卫旗
+ * @创建时间： @19/06/2018
+ */
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //DoSparseArray.testSparseArray();
+        //DoSparseArray.testMemoryTime();
+        DoHashMap.testHashMap();
+    }
+}
